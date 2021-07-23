2faf50883c50e5f13adf8d1e83b81e0f92c3eeed
Rodolfo Rodrigues Silva <rodolfo.rodrigues@zup.com.br>
2021-07-23 15:23:16 -0300

    Subindo classe endereco, imovel

---
+++ src/Imovel.java
@@ -0,0 +1,60 @@
+import java.util.List;
+
+public class Imovel {
+    private Endereco endereco;
+    private double valorAluguel;
+    private String nomeFuncionario;
+    private List<Morador> moradores;
+
+    public Imovel() {
+    }
+
+    public Imovel(Endereco endereco, double valorAluguel, String nomeFuncionario, List<Morador> moradores) {
+        this.endereco = endereco;
+        this.valorAluguel = valorAluguel;
+        this.nomeFuncionario = nomeFuncionario;
+        this.moradores = moradores;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public double getValorAluguel() {
+        return valorAluguel;
+    }
+
+    public void setValorAluguel(double valorAluguel) {
+        this.valorAluguel = valorAluguel;
+    }
+
+    public String getNomeFuncionario() {
+        return nomeFuncionario;
+    }
+
+    public void setNomeFuncionario(String nomeFuncionario) {
+        this.nomeFuncionario = nomeFuncionario;
+    }
+
+    public List<Morador> getMoradores() {
+        return moradores;
+    }
+
+    public void setMoradores(List<Morador> moradores) {
+        this.moradores = moradores;
+    }
+
+    @Override
+    public String toString() {
+        return "Imovel{" +
+                "endereco=" + endereco +
+                ", valorAluguel=" + valorAluguel +
+                ", nomeFuncionario='" + nomeFuncionario + '\'' +
+                ", moradores=" + moradores +
+                '}';
+    }
+}
